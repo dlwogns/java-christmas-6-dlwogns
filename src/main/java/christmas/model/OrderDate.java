823a6236b05ed17dc5fe837b3cf1315ee940513b
leejaehun <ldyjka@gmail.com>
2023-11-11 03:43:48 +0900

    feat(OrderDate.java): 식당 예상 방문 날짜 범위 검증 기능 추가 - add validateOrderdateRange in OrderDate.java

--- src/main/java/christmas/model/OrderDate.java
+++ src/main/java/christmas/model/OrderDate.java
@@ -12,9 +12,16 @@ public class OrderDate {
     }
     private void validate(String orderDate) throws IllegalArgumentException{
         validateOrderdateIsNumber(orderDate);
+        validateOrderdateRange(orderDate);
     }
     private void validateOrderdateIsNumber(String orderDate){
         if(!Pattern.matches(REGEX_DIGIT, orderDate)){
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateOrderdateRange(String orderDate){
+        int date = Integer.parseInt(orderDate);
+        if(date < 1 || date > 31){
             throw new IllegalArgumentException();
         }
     }
