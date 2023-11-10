2440e8bab5f7e521bd67050365fabac1f8fb9ee3
leejaehun <ldyjka@gmail.com>
2023-11-11 03:37:56 +0900

    feat(Order.java, OrderDate.java): 식당 예상 방문 날짜 숫자만 입력 가능 처리 - add validateOrderdateIsNumber in OrderDate.java

--- src/main/java/christmas/model/OrderDate.java
+++ src/main/java/christmas/model/OrderDate.java
@@ -1,9 +1,21 @@
 package christmas.model;
 
+import java.util.regex.Pattern;
+
 public class OrderDate {
     private final Integer orderDate;
+    private final String REGEX_DIGIT = "^[\\d]*$";
 
     public OrderDate(String orderDate) {
+        validate(orderDate);
         this.orderDate = Integer.parseInt(orderDate);
+    }
+    private void validate(String orderDate) throws IllegalArgumentException{
+        validateOrderdateIsNumber(orderDate);
+    }
+    private void validateOrderdateIsNumber(String orderDate){
+        if(!Pattern.matches(REGEX_DIGIT, orderDate)){
+            throw new IllegalArgumentException();
+        }
     }
 }
