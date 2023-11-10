e7c6a5d1e24858e8f61c2c4c9b8b89f1eb790724
leejaehun <ldyjka@gmail.com>
2023-11-11 03:25:08 +0900

    feat(OutputView.java): 시작 멘트 출력 - add printHelloMessage in OutputView.java

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,4 +1,14 @@
 package christmas.view;
 
 public class OutputView {
+    private static OutputView instance;
+    public static  OutputView getInstance(){
+        if(instance == null)
+            return instance = new OutputView();
+        return instance;
+    }
+
+    public void printHelloMessage(){
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
 }
