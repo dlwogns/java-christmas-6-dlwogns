c6565bd927b75db9f1f32abeac915e62908ce182
leejaehun <ldyjka@gmail.com>
2023-11-11 03:29:33 +0900

    refactor(InputView.java): return 값 수정 - return input; to return Console.readLine();

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -12,7 +12,6 @@ public class InputView {
 
     public String readDate(){
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
-        return input;
+        return Console.readLine();
     }
 }
