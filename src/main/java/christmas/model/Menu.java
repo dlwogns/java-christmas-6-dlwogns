5bc2fb79fc4b0459febfed2b8f9b29524cd8bbdc
leejaehun <ldyjka@gmail.com>
2023-11-11 19:38:17 +0900

    feat(Menu.java): 메뉴 형식에 안맞을 경우 예외처리 - add validateMenuFormat in Menu.java

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -1,11 +1,23 @@
 package christmas.model;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Menu {
     private final String menu;
+    private final String MENU_FORMAT = "^[가-힣]*-\\d$";
 
-    public Menu(String menu) {
+    public Menu(final String menu) {
+        validate(menu);
         this.menu = menu;
     }
+    private void validate(final String menu){
+        validateMenuFormat(menu);
+    }
+    private void validateMenuFormat(final String menu){
+        if(!Pattern.matches(MENU_FORMAT, menu)){
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
