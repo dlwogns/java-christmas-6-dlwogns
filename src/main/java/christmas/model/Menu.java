ac0d6a931ef1c84c6aa2a71e53cb0fb6fe394b4e
leejaehun <ldyjka@gmail.com>
2023-11-11 20:11:08 +0900

    feat(Menu.java): 메뉴 문자열 받아서 메뉴, 가격 형식으로 변환 - add parseMenu in Menu.java

--- src/main/java/christmas/model/Menu.java
+++ src/main/java/christmas/model/Menu.java
@@ -1,18 +1,23 @@
 package christmas.model;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Menu {
-    private final String menu;
-    private final String MENU_FORMAT = "^[가-힣]*-\\d$";
+    private final Map<String,Integer> menu;
+    private final String MENU_FORMAT = "^([가-힣]+-\\d+)(,([가-힣]+-\\d+))*";
 
     public Menu(final String menu) {
-        validate(menu);
-        this.menu = menu;
+        validateMenuFormat(menu);
+        this.menu = parseMenu(menu);
     }
     private void validate(final String menu){
         validateMenuFormat(menu);
+
     }
     private void validateMenuFormat(final String menu){
         if(!Pattern.matches(MENU_FORMAT, menu)){
@@ -20,4 +25,22 @@ public class Menu {
         }
     }
 
+    private Map<String,Integer> parseMenu(String menu){
+        return parseMenuByDash(parseMenuByComma(menu));
+    }
+
+    private List<String> parseMenuByComma(String menu){
+        return Arrays.stream(menu.split(","))
+                .collect(Collectors.toList());
+    }
+    private Map<String, Integer> parseMenuByDash(List<String> menu){
+        return menu.stream()
+                .map(s -> s.split("-"))
+                .collect(Collectors.toMap(arr->arr[0], arr -> Integer.parseInt(arr[1])));
+    }
+
+    @Override
+    public String toString() {
+        return menu.toString();
+    }
 }
