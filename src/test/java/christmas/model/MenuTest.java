fb385a130cc465287f727a919a39d925a1a6f1c0
leejaehun <ldyjka@gmail.com>
2023-11-11 21:04:00 +0900

    test(MenuTest.java): Menu 예외처리 테스트 추가 : add test in MenuTest.java

---
+++ src/test/java/christmas/model/MenuTest.java
@@ -0,0 +1,30 @@
+package christmas.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.Test;
+
+class MenuTest {
+    @Test
+    void 매뉴_형식_확인(){
+        assertThatThrownBy(() -> new Menu("봉골레파스타1,피자-2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴개수_1_이상인지_확인(){
+        assertThatThrownBy(() -> new Menu("파스타-0"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void 중복메뉴_입력_확인(){
+        assertThatThrownBy(() -> new Menu("파스타-1,파스타-2"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+    @Test
+    void 메뉴개수_20개이상_체크(){
+        assertThatThrownBy(() -> new Menu("파스타-10,피자-20,햄버거-30"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
