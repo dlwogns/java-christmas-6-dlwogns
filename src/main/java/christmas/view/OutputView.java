5b76332cdd24b30de549cf9cec8104ef26936955
leejaehun <ldyjka@gmail.com>
2023-11-13 04:58:02 +0900

    feat(OutputView.java): 이벤트 관련 단순출력 메세지 출력 - add printEventMessage in OutputView.java

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -11,4 +11,7 @@ public class OutputView {
     public void printHelloMessage(){
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
+    public void printEventMessage(){
+        System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
 }
