b612cbe95850aeab2f2a9c1e7f6b9a423b30df56
leejaehun <ldyjka@gmail.com>
2023-11-13 05:30:22 +0900

    test(OrderAmountTest.java): 총주문금액 체크 테스트 추가 - add 총주문금액_체크 in OrderAmountTest.java

---
+++ src/test/java/christmas/model/OrderAmountTest.java
@@ -0,0 +1,14 @@
+package christmas.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderAmountTest {
+    @Test
+    void 총주문금액_체크(){
+        OrderAmount orderAmount = new OrderAmount(new Menu("티본스테이크-1,제로콜라-1,양송이스프-2"));
+        assertThat(orderAmount.getOrderAmount()).isEqualTo(70000);
+    }
+}
