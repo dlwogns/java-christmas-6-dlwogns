c95fab6bde56b0a847857157e6e14380d27e9691
leejaehun <ldyjka@gmail.com>
2023-11-13 17:12:42 +0900

    feat(Badge.java): 이벤트 뱃지 상수 클래스 생성 - make Badge.java

---
+++ src/main/java/christmas/constant/Badge.java
@@ -0,0 +1,17 @@
+package christmas.constant;
+
+public enum Badge {
+    STAR("별"),
+    TREE("트리"),
+    SANTA("산타"),
+    NONE("없음")
+    ;
+    private final String badge;
+
+    Badge(String badge) {
+        this.badge = badge;
+    }
+    public String getValue(){
+        return badge;
+    }
+}
