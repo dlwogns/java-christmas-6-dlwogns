59ac6db592f7ca2bd3079ba518ab1db48dd03a4f
leejaehun <ldyjka@gmail.com>
2023-11-13 09:40:48 +0900

    feat(OutputView.java): 메뉴 출력기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,6 +1,7 @@
 package christmas.view;
 
 import christmas.model.Discount;
+import christmas.model.Menu;
 import christmas.model.OrderAmount;
 import org.mockito.internal.matchers.Or;
 
@@ -15,8 +16,10 @@ public class OutputView {
     public void printHelloMessage(){
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
-    public void printEventMessage(){
+    public void printMenu(Menu menu){
         System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println("<주문 메뉴>");
+        System.out.println(menu.toString());
     }
     public void printOrderAmount(OrderAmount orderAmount){
         System.out.println("<할인 전 총주문 금액>");
