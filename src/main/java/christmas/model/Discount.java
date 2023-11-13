43abd46bc40e258d70d7ea7ee345c672653123b1
leejaehun <ldyjka@gmail.com>
2023-11-13 09:41:36 +0900

    feat(Discount.java): 전체 할인 금액 계산 기능 추가, toString 추가 - add calculateTotalDiscount, toString

--- src/main/java/christmas/model/Discount.java
+++ src/main/java/christmas/model/Discount.java
@@ -5,11 +5,13 @@ public class Discount {
     private Integer weekDayDiscount;
     private Integer weekEndDiscount;
     private Integer starDiscount;
+    private Integer totalDiscount;
     public Discount(MenuChecker menuChecker, OrderDate orderDate){
         dDayDiscount = calculatedDayDiscount(orderDate);
         weekDayDiscount = calculateWeekDayDiscount(menuChecker,orderDate);
         weekEndDiscount = calculateWeekEndDiscount(menuChecker,orderDate);
         starDiscount = calculateStarDayDiscount(orderDate);
+        totalDiscount = calculateTotalDiscount();
     }
     private Integer calculatedDayDiscount(OrderDate orderDate){
         if(orderDate.getOrderDate() > 25)
@@ -30,5 +32,21 @@ public class Discount {
         if(orderDate.checkStarDay() == false)
             return 0;
         return 1000;
+    }
+    private Integer calculateTotalDiscount(){
+        return dDayDiscount+weekDayDiscount+weekEndDiscount+starDiscount;
+    }
+
+    @Override
+    public String toString() {
+        String discount = "<혜택 내역>\n";
+        if(dDayDiscount != 0)
+            discount += "크리스마스 디데이 할인: -" + dDayDiscount + '\n';
+        if(weekDayDiscount != 0)
+            discount += "평일 할인: -" + weekDayDiscount + '\n';
+        if(weekEndDiscount != 0)
+            discount += "주말 할인: -" + weekEndDiscount +'\n';
+
+        return discount;
     }
 }
