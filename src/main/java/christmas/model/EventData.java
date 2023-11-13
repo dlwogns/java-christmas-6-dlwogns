86fab7742425ec342100b1372573d0b730da32fa
leejaehun <ldyjka@gmail.com>
2023-11-13 10:06:47 +0900

    feat(EventData.java): 샴페인 체크 기능 추가 - add checkChampagne

--- src/main/java/christmas/model/EventData.java
+++ src/main/java/christmas/model/EventData.java
@@ -1,17 +1,19 @@
 package christmas.model;
 
 public class EventData {
-    private Integer dDayDiscount;
-    private Integer weekDayDiscount;
-    private Integer weekEndDiscount;
-    private Integer starDiscount;
-    private Integer totalDiscount;
-    public EventData(MenuChecker menuChecker, OrderDate orderDate){
+    private final Integer dDayDiscount;
+    private final Integer weekDayDiscount;
+    private final Integer weekEndDiscount;
+    private final Integer starDiscount;
+    private final Integer totalDiscount;
+    private final boolean champagne;
+    public EventData(MenuChecker menuChecker, OrderDate orderDate, OrderAmount orderAmount){
         dDayDiscount = calculatedDayDiscount(orderDate);
         weekDayDiscount = calculateWeekDayDiscount(menuChecker,orderDate);
         weekEndDiscount = calculateWeekEndDiscount(menuChecker,orderDate);
         starDiscount = calculateStarDayDiscount(orderDate);
         totalDiscount = calculateTotalDiscount();
+        champagne = checkChampagne(orderAmount);
     }
     private Integer calculatedDayDiscount(OrderDate orderDate){
         if(orderDate.getOrderDate() > 25)
@@ -36,17 +38,27 @@ public class EventData {
     private Integer calculateTotalDiscount(){
         return dDayDiscount+weekDayDiscount+weekEndDiscount+starDiscount;
     }
+    private boolean checkChampagne(OrderAmount orderAmount){
+        return orderAmount.getOrderAmount() >= 120000;
+    }
 
     @Override
     public String toString() {
-        String discount = "<혜택 내역>\n";
+        String discount = toStringifChampagne();
+        discount += "<혜택 내역>\n";
         if(dDayDiscount != 0)
             discount += "크리스마스 디데이 할인: -" + dDayDiscount + '\n';
         if(weekDayDiscount != 0)
             discount += "평일 할인: -" + weekDayDiscount + '\n';
         if(weekEndDiscount != 0)
             discount += "주말 할인: -" + weekEndDiscount +'\n';
-
+        if(champagne)
+            discount += "증정 이벤트: -" + "25000" + '\n';
         return discount;
+    }
+    private String toStringifChampagne(){
+        if(champagne)
+            return "<증정 메뉴>\n샴페인 1개\n";
+        return "";
     }
 }
